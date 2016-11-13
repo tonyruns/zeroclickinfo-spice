8e50a7d80fb1c30e7f5644d32826d1dbe0f81a28
Dweep Shah <dweepcan1@msn.com>
2016-11-13 02:01:29 +0000

    Add support for multiple

--- lib/DDG/Spice/ImdbMovies.pm
+++ lib/DDG/Spice/ImdbMovies.pm
@@ -1,6 +1,6 @@
 package DDG::Spice::ImdbMovies;
 
-# ABSTRACT: Write an abstract here
+# ABSTRACT: Movie information from IMDB
 
 # Start at http://docs.duckduckhack.com/walkthroughs/forum-lookup.html if you are new
 # to instant answer development
@@ -16,20 +16,22 @@ spice proxy_cache_valid => '200 1d'; # defaults to this automatically
 spice wrap_jsonp_callback => 1; # only enable for non-JSONP APIs (i.e. no &callback= parameter)
 
 # API endpoint - http://docs.duckduckhack.com/walkthroughs/forum-lookup.html#api-endpoint
-spice to => 'http://www.omdbapi.com/?t=$1&y=&plot=short&r=json';
+spice to => 'http://www.omdbapi.com/?s=$1&type=movie&r=json';
 
 # Triggers - https://duck.co/duckduckhack/spice_triggers
-triggers end => 'ratings', 'film';
+triggers end => 'ratings';
 triggers startend => 'imdb';
 
 # Handle statement
 handle remainder => sub {
-
+    # Return nothing if empty string
+    return unless $_;
+    
     # Query is in $_ or @_, depending on the handle you chose...if you
     # need to do something with it before returning
     my $find = "%20";
     my $replace = "+";
-    my $title = join( $replace, split($find, $_) );
+    my $title = join($replace, split($find, $_));
     return $title;
 };
 
--- share/spice/imdb_movies/imdb_movies.js
+++ share/spice/imdb_movies/imdb_movies.js
@@ -4,20 +4,28 @@
     env.ddg_spice_imdb_movies = function(api_result) {
         console.log(api_result);
         // Validate the response (customize for your Spice)
-        if (!api_result || api_result.error || api_result.Response == "False" ) {
+        if (!api_result || api_result.Response == "False" || api_result.Error || !api_result.Search.length) {
             return Spice.failed('imdb_movies');
         }
+        
+        // Get original query.
+        var script = $('[src*="/js/spice/imdb_movies/"]')[0],
+            source = $(script).attr("src"),
+            query = decodeURIComponent(source.match(/imdb_movies\/([^\/]+)/)[1]);
 
+        
         // Render the response
         Spice.add({
             id: 'imdb_movies',
 
             // Customize these properties
             name: 'Movies',
-            data: api_result,
+            data: api_result.Search,
             meta: {
                 sourceName: 'IMDb',
-                sourceUrl: ''
+                sourceUrl: 'http://www.imdb.com/find?ref_=nv_sr_fn&q=' + encodeURIComponent(query) + '&s=all',
+                searchTerm: query,
+                itemType: 'Movies'
             },
             normalize: function(item) {
                 var movieRating = parseFloat(item.imdbRating)/2.0;
