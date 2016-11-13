35b71d652c457d0ba116361cdd84e20ed2afa4d7
Zal Machado <zalmachado@gmail.com>
2016-11-12 21:06:42 -0500

    add film because its a legitimate term

--- lib/DDG/Spice/ImdbMovies.pm
+++ lib/DDG/Spice/ImdbMovies.pm
@@ -19,14 +19,14 @@ spice wrap_jsonp_callback => 1; # only enable for non-JSONP APIs (i.e. no &callb
 spice to => 'http://www.omdbapi.com/?s=$1&type=movie&r=json';
 
 # Triggers - https://duck.co/duckduckhack/spice_triggers
-triggers end => 'ratings';
+triggers end => 'ratings', 'film';
 triggers startend => 'imdb';
 
 # Handle statement
 handle remainder => sub {
     # Return nothing if empty string
     return unless $_;
-    
+
     # Query is in $_ or @_, depending on the handle you chose...if you
     # need to do something with it before returning
     my $find = "%20";
